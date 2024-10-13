d9a51f4e59eba372b92fa1effd3a1e4ddc2edff2
viktoriiatsymbal <149361278+viktoriiatsymbal@users.noreply.github.com>
2024-10-13 21:51:15 +0300

    Created FlowerTypeAndColor.java

---
+++ src/main/java/flower/store/FlowerTypeAndColor.java
@@ -0,0 +1,15 @@
+package flower.store;
+
+public class FlowerTypeAndColor {
+    private FlowerType flowerType;
+    private FlowerColor colour;
+
+    public FlowerTypeAndColor(FlowerType type, FlowerColor color) {
+        flowerType = type;
+        colour = color;
+    }
+
+    public Boolean matches(FlowerTypeAndColor tandc) {
+        return flowerType.equals(tandc.flowerType) && colour.equals(tandc.colour);
+    }
+}
