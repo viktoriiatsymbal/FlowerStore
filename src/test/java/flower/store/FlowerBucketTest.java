61d76cd84e25f757810466e6f85e587cc5e6dc4b
viktoriiatsymbal <149361278+viktoriiatsymbal@users.noreply.github.com>
2024-10-13 22:41:04 +0300

    fixed magic number in FlowerBucketTest class

--- src/test/java/flower/store/FlowerBucketTest.java
+++ src/test/java/flower/store/FlowerBucketTest.java
@@ -10,6 +10,7 @@ public class FlowerBucketTest {
     private static final Random RANDOM_GENERATOR = new Random();
     private static final int MAX_QUANTITY = 1000;
     private static final int MAX_PRICE = 100;
+    private static final int NEW_PRICE = 10;
 
     private FlowerBucket flowerBucket;
 
@@ -23,7 +24,7 @@ public class FlowerBucketTest {
         int price = RANDOM_GENERATOR.nextInt(MAX_PRICE);
         int quantity = RANDOM_GENERATOR.nextInt(MAX_QUANTITY);
         Flower flower = new Rose();
-        flower.setPrice(10);
+        flower.setPrice(NEW_PRICE);
         FlowerPack flowerPack = new FlowerPack(flower, quantity);
         flowerBucket.add(flowerPack);
         Assertions.assertEquals(price * quantity, flowerBucket.getPrice());
